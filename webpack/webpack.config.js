c3519c7ee752ac2aa2d1ed7f60ad428bf6d8c1b9
Khoa Nguyen <n.d.khoa511@gmail.com>
2024-07-31 13:12:33 +0700

    init project set up webpack & babel

---
+++ webpack/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "..", "./src/index.js"),
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "..", "./build"),
+    filename: "bundle.js",
+  },
+  target: ["web", "es5"], // default value
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./src/index.html"),
+    }),
+  ],
+};
